7f9dabc6e728f3f84667011d32005b7e3d8965bd
Your Name <you@example.com>
2024-06-03 15:32:58 +0900

    add:掲示板作成処理を実装

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -5,4 +5,20 @@ class BoardsController < ApplicationController
   def new
     @board = Board.new
   end
+  def create
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      redirect_to boards_path, success: t('defaults.flash_message.created', item: Board.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.flash_message.not_created', item: Board.model_name.human)
+      render :new, status: :unprocessable_entity
+    end
+  end
+end
+
+private
+
+ def board_params
+  params.require(:board).permit(:title, :body)
+ end
 end
