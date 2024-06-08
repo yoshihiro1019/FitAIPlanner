7d8d51780ebe6689899f6195a2b465ca689c8b0f
Your Name <you@example.com>
2024-06-08 14:18:25 +0900

    11 掲示板詳細画面の追加/コメント機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,6 @@ class User < ApplicationRecord
   has_many :boards, dependent: :destroy
   has_many :comments, dependent: :destroy
   def full_name
-    "#{first_name} #{last_name}"
+    "#{last_name} #{first_name}"
   end
 end
