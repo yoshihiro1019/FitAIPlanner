9ece32bfad6f6b275a333c9f73d7f0ef2102fc91
Your Name <you@example.com>
2024-06-08 11:19:29 +0900

    11 掲示板詳細画面の追加/コメント機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_07_062512) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_07_113007) do
   create_table "boards", charset: "utf8mb4", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
@@ -39,7 +39,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_07_062512) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "password_confirmation"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "boards", "users"
