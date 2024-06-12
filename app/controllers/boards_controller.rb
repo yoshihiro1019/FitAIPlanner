efb5c4ba0654ccf5e710afd135a2a338ce802a61
Your Name <you@example.com>
2024-06-12 15:06:19 +0900

    13回目

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -7,8 +7,6 @@ class BoardsController < ApplicationController
   end
 
   def show
-    # URLパラメーターから掲示板のIDを取得
-    @board = Board.find(params[:id])
     @user = @board.user
   end
 
@@ -16,28 +14,15 @@ class BoardsController < ApplicationController
     @board = Board.new
   end
 
-  def edit
-    @board = Board.find(params[:id])
-    authorize_user!
-  end
+  def edit; end
 
   def create
     @board = current_user.boards.build(board_params)
     if @board.save
-      update_board_image if params[:board][:board_image].present?
       redirect_to boards_path, success: t('defaults.flash_message.created', item: Board.model_name.human)
     else
       flash.now[:danger] = t('defaults.flash_message.not_created', item: Board.model_name.human)
       render :new, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @board = Board.find(params[:id])
-    @board.destroy
-    respond_to do |format|
-      format.html { redirect_to boards_url, success: '投稿が削除されました。' }
-      format.json { head :no_content }
     end
   end
 
@@ -46,8 +31,16 @@ class BoardsController < ApplicationController
       flash[:success] = t('flash.success.board_updated')
       redirect_to @board
     else
-      flash.now[:alert] = t('flash.success.board_updated')
+      flash.now[:alert] = t('flash.alert.board_not_updated')
       render :edit
+    end
+  end
+
+  def destroy
+    @board.destroy
+    respond_to do |format|
+      format.html { redirect_to boards_url, success: '投稿が削除されました。' }
+      format.json { head :no_content }
     end
   end
 
@@ -64,7 +57,7 @@ class BoardsController < ApplicationController
   end
 
   def authorize_user!
-    return if @board.user != current_user
+    return if @board.user == current_user
 
     raise ActiveRecord::RecordNotFound
   end
