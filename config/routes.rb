3afb4aef91f0e1bb716561ae81de06d1b7cd1e52
Your Name <you@example.com>
2024-06-15 20:46:27 +0900

    14

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   root "static_pages#top"
+   get 'boards/bookmarks', to: 'bookmarks#index', as: 'bookmarks_boards'
   resources :users, only: %i[new create]
   resources :boards do
     resources :comments, shallow: true, only: %i[create edit update destroy]
     resource :bookmark, only: [:create, :destroy]
   end
-  
-  get 'bookmarks', to: 'bookmarks#index', as: 'bookmarks_boards'
+
+ 
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
