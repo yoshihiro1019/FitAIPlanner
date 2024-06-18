c22b1026e06e81541410f2f335ccbaebea906a9b
Your Name <you@example.com>
2024-06-18 20:58:16 +0900

    15 ブックマークボタンのajax化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
   add_flash_types :success, :danger
-  
+
   private
 
   def not_authenticated
--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,20 +4,12 @@ class BookmarksController < ApplicationController
   def create
     @board = Board.find(params[:board_id])
     current_user.bookmarks.create!(board: @board)
-    respond_to do |format|
-      format.turbo_stream
-      format.html { redirect_to boards_path, status: :see_other }
-    end
   end
 
   def destroy
     @board = Board.find(params[:board_id])
     bookmark = current_user.bookmarks.find_by(board: @board)
     bookmark&.destroy
-    respond_to do |format|
-      format.turbo_stream
-      format.html { redirect_to boards_path, status: :see_other }
-    end
   end
 
   def index
