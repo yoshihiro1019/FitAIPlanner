eca969774fce2352642fcbcd21624a379e0f4cae
Your Name <you@example.com>
2024-06-25 01:19:31 +0900

    19

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,30 +1,27 @@
 class ProfilesController < ApplicationController
-    before_action :set_user
-  
-    def show
+  before_action :set_user
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = 'ユーザーを更新しました'
+      redirect_to profile_path
+    else
+      flash.now[:danger] = 'ユーザーを更新出来ませんでした'
+      render :edit, status: :unprocessable_entity
     end
-  
-    def edit
-    end
-  
-    def update
-      if @user.update(user_params)
-        flash[:success] = "ユーザーを更新しました"
-        redirect_to profile_path
-      else
-        flash.now[:danger] = "ユーザーを更新出来ませんでした"
-        render :edit, status: :unprocessable_entity
-      end
-    end
-  
-    private
-  
-    def set_user
-      @user = current_user
-    end
-  
-    def user_params
-        params.require(:user).permit(:email, :first_name, :last_name, :avatar)
-      end
   end
-  
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :avatar)
+  end
+end
