e60c4b500c24ae54f439aaa777e3aaad90a389cd
Your Name <you@example.com>
2024-07-02 16:19:51 +0900

    20/6

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,10 +1,3 @@
-require "active_support/core_ext/integer/time"
-
-# The test environment is used exclusively to run your application's
-# test suite. You never need to work with it otherwise. Remember that
-# your test database is "scratch space" for the test suite and is wiped
-# and recreated between test runs. Don't rely on the data there!
-
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -43,7 +36,9 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
-  config.action_mailer.default_url_options = Settings.default_url_options.to_h
+  # ここで default_url_options を直接設定
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
