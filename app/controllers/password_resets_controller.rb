f764324b0dc6ef55cbad7cda9d9e33908bf8bf17
Your Name <you@example.com>
2024-07-02 17:05:51 +0900

    20.9

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,37 +1,33 @@
 class PasswordResetsController < ApplicationController
-    skip_before_action :require_login
-  
-    def new; end
-  
-    def create
-      @user = User.find_by(email: params[:email])
-      if @user
-        @user.deliver_reset_password_instructions!
-        redirect_to login_path, success: t('.success')
-      else
-        redirect_to login_path, success: t('.success') # セキュリティのため同じメッセージを表示
-      end
-    end
-  
-    def edit
-      @token = params[:id]
-      @user = User.load_from_reset_password_token(@token)
-      not_authenticated if @user.blank?
-    end
-  
-    def update
-      @token = params[:id]
-      @user = User.load_from_reset_password_token(@token)
-  
-      return not_authenticated if @user.blank?
-  
-      @user.password_confirmation = params[:user][:password_confirmation]
-      if @user.change_password(params[:user][:password])
-        redirect_to login_path, success: t('.success')
-      else
-        flash.now[:danger] = t('.fail')
-        render :edit, status: :unprocessable_entity
-      end
+  skip_before_action :require_login
+
+  def new; end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    @user&.deliver_reset_password_instructions!
+    # セキュリティのため、存在しないメールアドレスの場合も成功メッセージを表示
+    redirect_to login_path, success: t('.success')
+  end
+
+  def edit
+    @token = params[:id]
+    @user = User.load_from_reset_password_token(@token)
+    not_authenticated if @user.blank?
+  end
+
+  def update
+    @token = params[:id]
+    @user = User.load_from_reset_password_token(@token)
+
+    return not_authenticated if @user.blank?
+
+    @user.password_confirmation = params[:user][:password_confirmation]
+    if @user.change_password(params[:user][:password])
+      redirect_to login_path, success: t('.success')
+    else
+      flash.now[:danger] = t('.fail')
+      render :edit, status: :unprocessable_entity
     end
   end
-  
+end
