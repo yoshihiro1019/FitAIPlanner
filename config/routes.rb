f919f981aadde253220c1e9d1ae2500818eb679e
Your Name <you@example.com>
2024-07-02 15:24:42 +0900

    20

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   root "static_pages#top"
   
   get 'boards/bookmarks', to: 'bookmarks#index', as: 'bookmarks_boards'
@@ -8,7 +9,7 @@ Rails.application.routes.draw do
     resource :bookmark, only: [:create, :destroy]
   end  
   resource :profile, only: [:show, :edit, :update]
-
+  resources :password_resets, only: %i[new create edit update]
   
 
   get 'login', to: 'user_sessions#new'
--- db/schema.rb
+++ db/schema.rb
@@ -75,11 +75,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_24_070740) do
     t.string "last_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
-    t.string "password_confirmation"
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.integer "access_count_to_reset_password_page", default: 0
     t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
